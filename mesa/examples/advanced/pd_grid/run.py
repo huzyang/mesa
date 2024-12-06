4b3ddab4664449faa8954b09646111ecf1a69723
huzyang <huzyang57@163.com>
2024-12-06 11:23:08 +0800

    修改run.py文件 	modified:   mesa/examples/advanced/pd_grid/run.py 	modified:   mesa/examples/basic/start_model/run.py

--- mesa/examples/advanced/pd_grid/run.py
+++ mesa/examples/advanced/pd_grid/run.py
@@ -7,7 +7,7 @@ bwr = plt.get_cmap("bwr")
 seed = 21
 
 
-def draw_grid(model, ax=None):
+def draw_grid(model: PdGrid, ax=None):
     """
     Draw the current state of the grid, with Defecting agents in red
     and Cooperating agents in blue.
@@ -15,7 +15,9 @@ def draw_grid(model, ax=None):
     if not ax:
         fig, ax = plt.subplots(figsize=(6, 6))
     grid = np.zeros((model.grid.width, model.grid.height))
-    for agent, (x, y) in model.grid.coord_iter():
+    for cell in model.grid.all_cells.cells:
+        agent = cell.agents[0]
+        (x, y) = cell.coordinate
         if agent.move == "D":
             grid[y][x] = 1
         else:
@@ -36,10 +38,10 @@ def run_model(model):
     ax3 = fig.add_subplot(233)
     ax4 = fig.add_subplot(212)
 
-    # draw_grid(model, ax1)
+    draw_grid(model, ax1)
     model.run(10)
     # draw_grid(model, ax2)
-    model.run(10)
+    # model.run(10)
     # draw_grid(model, ax3)
     model.datacollector.get_model_vars_dataframe().plot(ax=ax4)
 
--- mesa/examples/basic/start_model/run.py
+++ mesa/examples/basic/start_model/run.py
@@ -1,4 +1,6 @@
 from model import PdGrid
+# Data visualization tools.
+import seaborn as sns
 
 # Set the random seed
 seed = 21
@@ -10,3 +12,15 @@ def run_model(model):
 
 m = PdGrid(10, 10, "Random", seed=seed)
 run_model(m)
+
+agent_score = [a.score for a in m.agents]
+# Create a histogram with seaborn
+g1 = sns.histplot(agent_score, discrete=True)
+g1.set(
+    title="score distribution", xlabel="score", ylabel="number of agents"
+);  # The semicolon is just to avoid printing the object representation
+
+fra_c = m.datacollector.get_model_vars_dataframe()
+# Plot the fra_c over time
+g2 = sns.lineplot(data=fra_c)
+g2.set(title="fra_c over Time", ylabel="fra_c")

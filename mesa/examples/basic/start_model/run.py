130ff92d9bd6d31703af34bb524d0edf8496ba50
huzyang <huzyang57@163.com>
2024-12-06 17:32:39 +0800

    modified:   mesa/examples/basic/start_model/run.py

--- mesa/examples/basic/start_model/run.py
+++ mesa/examples/basic/start_model/run.py
@@ -1,6 +1,8 @@
 from model import PdGrid
+
 # Data visualization tools.
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Set the random seed
 seed = 21
@@ -13,14 +15,14 @@ def run_model(model):
 m = PdGrid(10, 10, "Random", seed=seed)
 run_model(m)
 
-agent_score = [a.score for a in m.agents]
-# Create a histogram with seaborn
-g1 = sns.histplot(agent_score, discrete=True)
-g1.set(
-    title="score distribution", xlabel="score", ylabel="number of agents"
-);  # The semicolon is just to avoid printing the object representation
+# agent-score data
+# agent_score = m.datacollector.get_agent_vars_dataframe()
+# agent_score.head()
 
+# Model data
 fra_c = m.datacollector.get_model_vars_dataframe()
 # Plot the fra_c over time
 g2 = sns.lineplot(data=fra_c)
 g2.set(title="fra_c over Time", ylabel="fra_c")
+g2.set_ylim(0.3, 1)
+plt.show()

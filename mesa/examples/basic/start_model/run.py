26c2593b7b279c2a72da4b632a2635218923008c
huzyang <huzyang57@163.com>
2024-12-08 21:36:59 +0800

    benth run 	modified:   mesa/examples/basic/start_model/run.py

--- mesa/examples/basic/start_model/run.py
+++ mesa/examples/basic/start_model/run.py
@@ -3,26 +3,39 @@ from model import PdGrid
 # Data visualization tools.
 import seaborn as sns
 import matplotlib.pyplot as plt
+import mesa
 
 # Set the random seed
 seed = 21
 
 
 def run_model(model):
-    model.run(5)
+    model.run(10)
 
 
-m = PdGrid(10, 10, "Random", seed=seed)
-run_model(m)
+# m = PdGrid(10, 10, "Random", seed=seed)
+# run_model(m)
+params = {"width": range(10, 30, 10), "height": 10}
+results = mesa.batch_run(
+    PdGrid,
+    parameters=params,
+    iterations=5,
+    max_steps=10,
+    number_processes=1,
+    data_collection_period=1,
+    display_progress=True,
+)
 
-# agent-score data
-# agent_score = m.datacollector.get_agent_vars_dataframe()
-# agent_score.head()
+
+
+# Agent data
+# agent_score = [a.score for a in m.agents]
+# g1 = sns.histplot(agent_score, discrete=True)
+# g1.set(title="Score distribution", xlabel="Score", ylabel="number of agents")
 
 # Model data
-fra_c = m.datacollector.get_model_vars_dataframe()
-# Plot the fra_c over time
-g2 = sns.lineplot(data=fra_c)
-g2.set(title="fra_c over Time", ylabel="fra_c")
-g2.set_ylim(0.3, 1)
-plt.show()
+# fra_c = m.datacollector.get_model_vars_dataframe()
+# g2 = sns.lineplot(data=fra_c)
+# g2.set(title="fra_c over Time", ylabel="fra_c")
+# g2.set_ylim(0.3, 1.01)
+# plt.show()
